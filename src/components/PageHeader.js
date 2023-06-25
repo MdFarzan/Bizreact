b41a59bcde794e30399bd6fdd26051db8bd40db3
MdFarzan <mdfarzan776@gmail.com>
2023-06-25 13:42:54 +0530

    font weight added in headings

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,7 +1,7 @@
 export default function PageHeader(props) {
   return (
     <section className="py-4 text-center bg-dark text-white">
-      <h1>
+      <h1 className="mb-0">
         {props.title === "" || props.title == null ? "Page Title" : props.title}
       </h1>
     </section>
