77ada826da8ffc214ef999b642cabec1547df676
MdFarzan <mdfarzan776@gmail.com>
2023-06-25 10:45:23 +0530

    class error removed appearing in console

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -3,50 +3,83 @@ import "./CTA.css";
 import ContactForm from "./ContactForm";
 import LeftTitle from "./LeftTitle";
 
-export default function CTA(){
-    return (
-        <section className="mt-2 py-4 py-lg-5 cta">
-        <Container>
+export default function CTA() {
+  return (
+    <section className="mt-2 py-4 py-lg-5 cta">
+      <Container>
         <Row>
-            <Col md="6" lg="6" className="d-flex align-items-center">
+          <Col md="6" lg="6" className="d-flex align-items-center">
+            <Row>
+              <Col md={12} className="mb-5">
                 <Row>
-                    <Col md={12} className="mb-5">
-                        <Row>
-                            <Col md="2"><span class="icon d-block"><img className="img-fluid" src="services/ux.png" alt="icon" /></span></Col>
-                            <Col md="9">
-                            <h5 className="mb-0">Call Us</h5>
-                            <a class="heading" href="tel:+91 9999 999 999">+91 9999 999 999</a>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col md={12} className="mb-5">
-                        <Row>
-                            <Col md="2"><span class="icon d-block"><img className="img-fluid" src="services/ux.png" alt="icon" /></span></Col>
-                            <Col md="9">
-                            <h5 className="mb-0">Mail Us</h5>
-                            <a class="heading" href="mail:site@mail.com">site@mail.com</a>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col md={12} className="mb-5">
-                        <Row>
-                            <Col md="2"><span class="icon d-block"><img className="img-fluid" src="services/ux.png" alt="icon" /></span></Col>
-                            <Col md="9">
-                            <h5 className="mb-0">Reach Us</h5>
-                            <a class="heading" href="tel:+91 9999 999 999">44K Tower B, California, USA</a>
-                            </Col>
-                        </Row>
-                    </Col>
+                  <Col md="2">
+                    <span className="icon d-block">
+                      <img
+                        className="img-fluid"
+                        src="services/ux.png"
+                        alt="icon"
+                      />
+                    </span>
+                  </Col>
+                  <Col md="9">
+                    <h5 className="mb-0">Call Us</h5>
+                    <a className="heading" href="tel:+91 9999 999 999">
+                      +91 9999 999 999
+                    </a>
+                  </Col>
                 </Row>
-            </Col>
-            <Col md="6" lg="6" >
-            <LeftTitle title="Please fill this form" subtitle="Want a callback?"></LeftTitle>
+              </Col>
+              <Col md={12} className="mb-5">
+                <Row>
+                  <Col md="2">
+                    <span className="icon d-block">
+                      <img
+                        className="img-fluid"
+                        src="services/ux.png"
+                        alt="icon"
+                      />
+                    </span>
+                  </Col>
+                  <Col md="9">
+                    <h5 className="mb-0">Mail Us</h5>
+                    <a className="heading" href="mail:site@mail.com">
+                      site@mail.com
+                    </a>
+                  </Col>
+                </Row>
+              </Col>
+              <Col md={12} className="mb-5">
+                <Row>
+                  <Col md="2">
+                    <span className="icon d-block">
+                      <img
+                        className="img-fluid"
+                        src="services/ux.png"
+                        alt="icon"
+                      />
+                    </span>
+                  </Col>
+                  <Col md="9">
+                    <h5 className="mb-0">Reach Us</h5>
+                    <a className="heading" href="tel:+91 9999 999 999">
+                      44K Tower B, California, USA
+                    </a>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+          <Col md="6" lg="6">
+            <LeftTitle
+              title="Please fill this form"
+              subtitle="Want a callback?"
+            ></LeftTitle>
             <div className="mt-4">
-                <ContactForm />
-                </div>
-            </Col>
+              <ContactForm />
+            </div>
+          </Col>
         </Row>
-        </Container>
-        </section>
-    )
+      </Container>
+    </section>
+  );
 }
--- src/components/TestimonialSlider.js
+++ src/components/TestimonialSlider.js
@@ -3,7 +3,7 @@ import { Row } from "react-bootstrap";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './TestimonialSlider.css';
+import "./TestimonialSlider.css";
 
 export default class TestimonialSlider extends Component {
   render() {
@@ -13,111 +13,133 @@ export default class TestimonialSlider extends Component {
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
-      autoPlay: true
+      autoPlay: true,
     };
 
     return (
-        <section className="mt-2 py-4 py-lg-5" style={{background:'#f6f6f6'}}>
+      <section className="mt-2 py-4 py-lg-5" style={{ background: "#f6f6f6" }}>
         <div className="testimonial-wrap position-relative">
-        <img src="testimonial/left-quote.png"  className="position-absolute quote"/>
-        <Slider {...settings}>
-          <div className="w-100">
-            <div class="test-header text-center">
-                <div class="avtar-wrap text-center">
-                    <img src="services/ux.png" className="img-fluid mx-auto" />
+          <img
+            src="testimonial/left-quote.png"
+            className="position-absolute quote"
+          />
+          <Slider {...settings}>
+            <div className="w-100">
+              <div className="test-header text-center">
+                <div className="avtar-wrap text-center">
+                  <img src="services/ux.png" className="img-fluid mx-auto" />
                 </div>
                 <h4>Suresh Kumar</h4>
                 <h6>Designer</h6>
-                <p class="mt-3 px-3 px-md-4">
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                <p className="mt-3 px-3 px-md-4">
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters, as opposed to using 'Content
+                  here, content here', making it look like readable English.
                 </p>
                 <div className="star-rating d-flex justify-content-center mt-4">
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
                 </div>
+              </div>
             </div>
-          </div>
-          <div className="w-100">
-            <div class="test-header text-center">
-                <div class="avtar-wrap text-center">
-                    <img src="services/ux.png" className="img-fluid mx-auto" />
+            <div className="w-100">
+              <div className="test-header text-center">
+                <div className="avtar-wrap text-center">
+                  <img src="services/ux.png" className="img-fluid mx-auto" />
                 </div>
                 <h4>Suresh Kumar</h4>
                 <h6>Designer</h6>
-                <p class="mt-3 px-3 px-md-4">
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                <p className="mt-3 px-3 px-md-4">
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters, as opposed to using 'Content
+                  here, content here', making it look like readable English.
                 </p>
                 <div className="star-rating d-flex justify-content-center mt-4">
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
                 </div>
+              </div>
             </div>
-          </div>
-          <div className="w-100">
-            <div class="test-header text-center">
-                <div class="avtar-wrap text-center">
-                    <img src="services/ux.png" className="img-fluid mx-auto" />
+            <div className="w-100">
+              <div className="test-header text-center">
+                <div className="avtar-wrap text-center">
+                  <img src="services/ux.png" className="img-fluid mx-auto" />
                 </div>
                 <h4>Suresh Kumar</h4>
                 <h6>Designer</h6>
-                <p class="mt-3 px-3 px-md-4">
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                <p className="mt-3 px-3 px-md-4">
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters, as opposed to using 'Content
+                  here, content here', making it look like readable English.
                 </p>
                 <div className="star-rating d-flex justify-content-center mt-4">
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
                 </div>
+              </div>
             </div>
-          </div>
-          <div className="w-100">
-            <div class="test-header text-center">
-                <div class="avtar-wrap text-center">
-                    <img src="services/ux.png" className="img-fluid mx-auto" />
+            <div className="w-100">
+              <div className="test-header text-center">
+                <div className="avtar-wrap text-center">
+                  <img src="services/ux.png" className="img-fluid mx-auto" />
                 </div>
                 <h4>Suresh Kumar</h4>
                 <h6>Designer</h6>
-                <p class="mt-3 px-3 px-md-4">
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                <p className="mt-3 px-3 px-md-4">
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters, as opposed to using 'Content
+                  here, content here', making it look like readable English.
                 </p>
                 <div className="star-rating d-flex justify-content-center mt-4">
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
                 </div>
+              </div>
             </div>
-          </div>
-          <div className="w-100">
-            <div class="test-header text-center">
-                <div class="avtar-wrap text-center">
-                    <img src="services/ux.png" className="img-fluid mx-auto" />
+            <div className="w-100">
+              <div className="test-header text-center">
+                <div className="avtar-wrap text-center">
+                  <img src="services/ux.png" className="img-fluid mx-auto" />
                 </div>
                 <h4>Suresh Kumar</h4>
                 <h6>Designer</h6>
-                <p class="mt-3 px-3 px-md-4">
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                <p className="mt-3 px-3 px-md-4">
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters, as opposed to using 'Content
+                  here, content here', making it look like readable English.
                 </p>
                 <div className="star-rating d-flex justify-content-center mt-4">
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
-                <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
+                  <img src="testimonial/star.png" className="star-rating" />
                 </div>
+              </div>
             </div>
-          </div>
-          
-        </Slider>
+          </Slider>
         </div>
       </section>
     );
